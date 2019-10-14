05cbe8c50db83f32cc6a3db7865a0ee1eafd7d65
earthpanda <earthpanda@126.com>
2019-10-14 08:45:35 +0800

    nothing

--- versions/demon.py
+++ versions/demon.py
@@ -1,3 +1,11 @@
-from versions import filehelper
+import filehelper,os
 
 print(filehelper.getCurrentPath())
+
+
+filehelper.writeFileString(os.sep+"file"+os.sep+"test.txt","this is a test",True)
+
+filehelper.writeFileString(os.sep+"file"+os.sep+"test.txt","this is a test",True)
+
+filehelper.writeFileString(os.sep+"file"+os.sep+"test.txt","this is a test",False)
+
