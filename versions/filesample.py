0e194d0c504a4014b59354b388977f8e130c999f
lintao <lintao@znds.com>
2019-10-17 09:40:51 +0800

    nothing

--- versions/filesample.py
+++ versions/filesample.py
@@ -1,9 +1,12 @@
 import os
-from src.util.filehelper import write_file_string,read_file_string,rename_file,delete_file,copy_file
+from src.util.filehelper import write_file_string, read_file_string, rename_file, delete_file, copy_file
 from src.midground.file.midfile import delete_apks
+from src.entity.ApkEntity import *
+from src.util.apk_info_writer import *
+from src.apk.apk_info import *
+import src.util.time
 
-
-s=os.path.join(os.getcwd(),"file")
+s = os.path.join(os.getcwd(), "file")
 print(s)
 
 
@@ -22,10 +25,19 @@ print(s)
 # copy_file(os.path.join(s,"one","two","three","test.txt"),os.path.join(s,"one","two","three","test1.txt"))
 
 
-
-delete_apks(s)
-
+# delete_apks(s)
 
 
+def test():
+    apk_infos = ApkInfo()
+    apk1 = apk_infos.create_apk_entity("F1", "DBOS_F1", "launcher", "F12080", "2080", "100", "xc")
+    apk2 = apk_infos.create_apk_entity("C1", "DBOS_C1", "launcher", "C12080", "2080", "100", "xcc")
+    apk_infos.add_apk_entity(apk1)
+    apk_infos.add_apk_entity(apk2)
+
+    file_writer = ApkInfoWriter()
+    file_writer.write_to_file(apk_infos)
 
 
+if __name__ == '__main__':
+    test()
