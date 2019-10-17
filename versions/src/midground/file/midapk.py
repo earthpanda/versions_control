846eb00c040bdbfbf5bf01ef8a904d25eaeb44c4
yl <earthpanda@foxmail.com>
2019-10-17 18:02:45 +0800

    feature: 添加一些关于函数的定义

---
+++ versions/src/midground/file/midapk.py
@@ -0,0 +1,27 @@
+
+
+
+
+"""
+
+更新apk的信息
+
+"""
+def update_apk_infos():
+
+
+
+	pass
+
+"""
+
+得到最近一次的apk信息
+
+model 型号 F1 C1 D1 
+code 版本号 20800 20900
+
+"""
+def get_apk_infos(model,code):
+
+	pass	
+
